81ad44f62a53ce2c44b9d363d5af986362f0f016
CypherTrooper <jin360life@gmail.com>
2026-04-06 19:38:46 +0700

    Create colossusx_protocol.go

---
+++ node/node/colossusx_protocol.go
@@ -0,0 +1,12 @@
+package node
+
+import (
+	colossusx "github.com/cypherium/cypher/consensus/colossusx"
+	"github.com/cypherium/cypher/p2p"
+)
+
+// RegisterColossusXProtocol registers the colossusX/1 devp2p subprotocol.
+func (n *Node) RegisterColossusXProtocol(cfg colossusx.Config, handlers colossusx.Handlers) {
+	cfg.Logger = n.log
+	n.RegisterProtocols([]p2p.Protocol{colossusx.MakeProtocol(cfg, handlers)})
+}
