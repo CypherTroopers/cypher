4a1869dfa43f5fccb51d8ca80dec5a9cd34e72d7
CypherTrooper <jin360life@gmail.com>
2026-05-21 16:04:14 +0700

    Create Restart_windows.ps1

---
+++ Restart_windows.ps1
@@ -0,0 +1,29 @@
+# Restart_windows.ps1
+
+$ErrorActionPreference = "Stop"
+
+Write-Host "==> Get public IP"
+$PUBLIC_IP = (Invoke-RestMethod -Uri "https://api4.ipify.org").Trim()
+
+Write-Host "==> REstart Cypher node"
+.\build\bin\cypher.exe `
+  --verbosity 1 `
+  --rnetport 7200 `
+  --syncmode full `
+  --nat "extip:$PUBLIC_IP" `
+  --ws `
+  --ws.addr 0.0.0.0 `
+  --ws.port 9251 `
+  --ws.origins "*" `
+  --metrics `
+  --http `
+  --http.addr 0.0.0.0 `
+  --http.port 8000 `
+  --http.api eth,web3,net,txpool `
+  --http.corsdomain "*" `
+  --port 6000 `
+  --datadir .\chaindbname `
+  --networkid 123678 `
+  --gcmode archive `
+  --bootnodes "enode://e10a90e9c7d077002d4d56b88943b8dfbca1d6490bb92c8202e6acb68ef23b521bf187fb40c07eed2f453f3782e8c53ca5a4ec1d34a4454960143501df8c4b95@149.102.156.210:6000" `
+  console
