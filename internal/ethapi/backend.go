a43fb4435fdd8f455f9be46aec6bb556a3ba375c
CypherTrooper <181522906+CypherTroopers@users.noreply.github.com>
2026-06-08 05:12:06 +0700

    Register direct transaction receipt API override

--- internal/ethapi/backend.go
+++ internal/ethapi/backend.go
@@ -116,7 +116,7 @@ func GetAPIs(apiBackend Backend) []rpc.API {
 		}, {
 			Namespace: "eth",
 			Version:   "1.0",
-			Service:   NewPublicTransactionPoolAPIWithCommonReceipt(apiBackend, nonceLock),
+			Service:   NewPublicTransactionPoolAPI(apiBackend, nonceLock),
 			Public:    true,
 		}, {
 			Namespace: "txpool",
@@ -142,6 +142,11 @@ func GetAPIs(apiBackend Backend) []rpc.API {
 			Version:   "1.0",
 			Service:   NewPrivateAccountAPI(apiBackend, nonceLock),
 			Public:    false,
+		}, {
+			Namespace: "eth",
+			Version:   "1.0",
+			Service:   NewPublicTransactionReceiptAPI(apiBackend),
+			Public:    true,
 		},
 	}
 }
